e1f2ba169ca4c6bca5f1d02ba9639537019c1c58
WebdeployManager <162872496+WebdeployManager@users.noreply.github.com>
2024-03-30 23:54:46 +0900

    Update DeogcheonMap.js

--- Map/DeogcheonMap.js
+++ Map/DeogcheonMap.js
@@ -1,15 +1,18 @@
 var mapContainer4 = document.getElementById('deogcheon'),
-mapOption4 = { 
-	center: new kakao.maps.LatLng(35.2102581, 129.008073), 
-	level: 3 // 지도의 확대 레벨
-};
+  mapOption4 = {
+    center: new kakao.maps.LatLng(35.209481924828935, 129.0081822023491),
+    level: 3, // 지도의 확대 레벨
+  };
 
 var map4 = new kakao.maps.Map(mapContainer4, mapOption4);
 
-var markerPosition4  = new kakao.maps.LatLng(35.2102581, 129.008073); 
+var markerPosition4 = new kakao.maps.LatLng(
+  35.209481924828935,
+  129.0081822023491
+);
 
 var marker4 = new kakao.maps.Marker({
-	position: markerPosition4
+  position: markerPosition4,
 });
 
 marker4.setMap(map4);
