90689a7985113ce314584d8b035b605c8c57b5da
WebdeployManager <162872496+WebdeployManager@users.noreply.github.com>
2024-03-30 23:57:30 +0900

    Update DeogcheonMap.js

--- Map/DeogcheonMap.js
+++ Map/DeogcheonMap.js
@@ -1,7 +1,7 @@
 var mapContainer4 = document.getElementById('deogcheon'),
   mapOption4 = {
     center: new kakao.maps.LatLng(35.209481924828935, 129.0081822023491),
-    level: 2, // 지도의 확대 레벨
+    level: 4, // 지도의 확대 레벨
   };
 
 var map4 = new kakao.maps.Map(mapContainer4, mapOption4);
