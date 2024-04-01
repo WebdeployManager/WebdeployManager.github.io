c4545e447d459bcfefdd135163cd8d47d05e93e5
WebdeployManager <162872496+WebdeployManager@users.noreply.github.com>
2024-04-01 16:53:07 +0900

    Update YeonsanMap.js

--- Map/YeonsanMap.js
+++ Map/YeonsanMap.js
@@ -1,6 +1,6 @@
 var mapContainer10 = document.getElementById('Yeonsan'),
   mapOption10 = {
-    center: new kakao.maps.LatLng(35.18695519405669, 129.0844469352805),
+    center: new kakao.maps.LatLng(35.18695519405669, 129.08504861762057),
     level: 3, // 지도의 확대 레벨
   };
 
@@ -8,7 +8,7 @@ var map10 = new kakao.maps.Map(mapContainer10, mapOption10);
 
 var markerPosition10 = new kakao.maps.LatLng(
   35.18695519405669,
-  129.0844469352805
+  129.08504861762057
 );
 
 var marker10 = new kakao.maps.Marker({
